ecc5bbd0cfcaef6e91e93c16da4b2482837b21b3
Wojciech <WojciechM@yandex.com>
2021-08-17 16:52:27 +0200

    tresse

--- hola-wojciech.py
+++ hola-wojciech.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*- 
 """
-escribirname
+escribir script que pregunte el nombre del usario en la consola y un numero entero e imprima 
+por pantalla en lineas distintas el nombre del usario tantas veces como el numero introducido.
+
 """
 name = input("como te llamas:")
 n = input("introduce un numero entero: ")
-print ((name + "\n")*int(n))
+print ((name + "\n") * int(n))
